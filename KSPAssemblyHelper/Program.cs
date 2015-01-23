9a4f3d6f86e8b98dfdf1868b88a59dd6c8cf7383
Chaoslive <Chaoslive@users.noreply.github.com>
2015-01-23 17:27:29 +0900

    developpint composite class

--- KSPAssemblyHelper/Program.cs
+++ KSPAssemblyHelper/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
@@ -16,6 +17,15 @@ namespace KSPAssemblyHelper
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //以下テスト
+            Composite c1 = new Composite();
+            c1.Name = "Comp_1";
+            Composite c2 = new Composite();
+            c2.Name="Comp_2";
+            c1.AddComposite(c2);
+            c2.AddComposite(new Composite());
+            c1.PrintChildrenNames();
         }
     }
 }
